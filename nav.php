711c20aef0dd5025d51da9b9a7ca78d6ff408b49
Sophie Spang <soph8111@stud.kea.dk>
2021-10-19 16:50:00 +0200

    grid on homepage

---
+++ nav.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css">
+    <title>Homepage - Sophie Spang</title>
+</head>
+<body>
+
+    <nav>
+        <a class="elm" href="index.html"><img id="logo" src="./images/logo.png" alt="logo"></a>
+        <span class="elm">
+            <a href="#work">Work</a>
+            <a href="about.html">About</a>
+        </span>
+    </nav>
+
+
+    <!-- gsap -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/ScrollToPlugin.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/ScrollTrigger.min.js"></script>
+
+    <script src="script.js"></script>
+</body>
+</html>
--- script.js
+++ script.js
@@ -1,27 +1,57 @@
 "use strict";
 
-// https://css-tricks.com/scroll-drawing/
+window.addEventListener("DOMContentLoaded", start);
 
-// Get a reference to the <path>
-const path = document.querySelector("#guideline");
+function start() {
+  console.log("start");
 
-// Get total length of path
-const pathLength = path.getTotalLength();
-console.log(pathLength);
+  arrowDown();
 
-// Make very long dashes (the length of the path itself)
-path.style.strokeDasharray = pathLength + " " + pathLength;
+  startDrawingLine();
+}
 
-// Offset the dashes so the it appears hidden entirely
-path.style.strokeDashoffset = pathLength;
+function arrowDown() {
+  const properties = {
+    duration: 800,
+    iterations: Infinity,
+    direction: "alternate",
+    easing: "ease-out",
+  };
 
-// When the page scrolls...
-window.addEventListener("scroll", function (e) {
-  // What % down is it?
-  const scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-  // Length to offset the dashes
-  const drawLength = pathLength * scrollPercentage;
-  const newLegth = drawLength - 300;
-  // Draw in reverse
-  path.style.strokeDashoffset = pathLength - newLegth;
-});
+  const keyframes = [{ transform: `translate(0, 1vw)` }, { tranform: `transform: translate(0,0)` }];
+
+  const arrow = document.querySelector("#arrow_down");
+  const animation = arrow.animate(keyframes, properties);
+
+  arrow.addEventListener("click", () => {
+    gsap.to(window, { duration: 1, scrollTo: "#work" });
+  });
+}
+
+function startDrawingLine() {
+  // Draw line inspired by https://css-tricks.com/scroll-drawing/
+
+  // Get a reference to the <path>
+  const path = document.querySelector("#guideline");
+
+  // Get total length of path
+  const pathLength = path.getTotalLength();
+  //console.log(pathLength);
+
+  // Make very long dashes (the length of the path itself)
+  path.style.strokeDasharray = pathLength + " " + pathLength;
+
+  // Offset the dashes so the it appears hidden entirely
+  path.style.strokeDashoffset = pathLength;
+
+  // When the page scrolls...
+  window.addEventListener("scroll", function (e) {
+    // What % down is it?
+    const scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    // Length to offset the dashes
+    const drawLength = pathLength * scrollPercentage;
+    const newLength = drawLength - window.innerHeight / 2;
+    // Draw in reverse
+    path.style.strokeDashoffset = pathLength - newLength;
+  });
+}
