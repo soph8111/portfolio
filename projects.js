7e3c04ec32bc1e46c9970288002077960ccfbf16
Sophie Spang <soph8111@stud.kea.dk>
2021-10-22 21:38:52 +0200

    fade in function works on singleview

--- projects.js
+++ projects.js
@@ -1,18 +1,37 @@
-/* "use strict";
+"use strict";
 
 window.addEventListener("DOMContentLoaded", start);
 
 function start() {
   console.log("start");
 
-  projectHeader();
+  fadeIn();
 }
 
-function projectHeader() {
-  ScrollTrigger.create({
-    start: "top -100",
-    end: 99999,
-    toggleClass: { className: "--scrolled", targets: ".project_title" },
+function fadeIn() {
+  // Inspired by: https://www.youtube.com/watch?v=19jD-DcOBtQ
+
+  const firstObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        document.querySelector(".image1").classList.add("fade_in");
+      }
+    });
+  });
+
+  firstObserver.observe(document.querySelector(".images"));
+
+  const images = document.querySelectorAll(".image");
+
+  images.forEach((img) => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          img.classList.add("fade_in");
+        }
+      });
+    });
+
+    observer.observe(img);
   });
 }
- */
